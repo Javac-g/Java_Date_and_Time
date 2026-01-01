e3bac528e2ebfb541b07b98f1757e13ad19e1d9e
Javac-g <denysov.maxym@gmail.com>
2026-01-01 22:55:55 +0200

    Custom date 2

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
 
 public class Main {
 
     public static void main(String... args){
         LocalDate date1 = LocalDate.of(1992, 12,12);
+        LocalDate date2 = LocalDate.of(1222, Month.JULY, 17);
+        LocalDate date3 = LocalDate.now();
         System.out.println(date1);
+        System.out.println(date2);
+        System.out.println(date3);
     }
 }
