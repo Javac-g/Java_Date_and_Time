f5faa6fa5c36ae0075a3c370f9d28e5d6177a717
Javac-g <denysov.maxym@gmail.com>
2026-01-01 23:03:14 +0200

    LocalTime

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.Month;
 import java.time.Year;
 
@@ -8,8 +9,17 @@ public class Main {
         LocalDate date1 = LocalDate.of(1992, 12,12);
         LocalDate date2 = LocalDate.of(1222, Month.JULY, 17);
         LocalDate date3 = LocalDate.now();
+
+        LocalTime time1 = LocalTime.of(7,15);
+        LocalTime time2 = LocalTime.of(7,15,30);
+        LocalTime time3 = LocalTime.of(7,15,30,200);
+
         System.out.println(date1);
         System.out.println(date2);
         System.out.println(date3);
+
+        System.out.println(time1);
+        System.out.println(time2);
+        System.out.println(time3);
     }
 }
