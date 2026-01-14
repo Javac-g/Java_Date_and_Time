a1694d98dce1781a282e0f1d5306c62dd235b19c
Javac-g <denysov.maxym@gmail.com>
2026-01-14 23:11:56 +0200

    Local date training

--- TestTwo.java
+++ TestTwo.java
@@ -1,9 +1,15 @@
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.Month;
 
 public class TestTwo {
     LocalTime t1 = LocalTime.of(5,25);
     LocalTime t2 = LocalTime.of(5,25, 60);
     LocalTime t3 = LocalTime.of(5,25, 60,100);
     LocalTime t4 = LocalTime.now();
+
+    LocalDate d1 = LocalDate.of(2025,12,2);
+    LocalDate d2 = LocalDate.of(2025, Month.OCTOBER,2);
+    LocalDate d3 = LocalDate.now();
 
 }
