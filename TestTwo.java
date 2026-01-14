8afc961e441a3e6ab162c67e48c84681eb2e5079
Javac-g <denysov.maxym@gmail.com>
2026-01-14 23:09:12 +0200

    Local time training

---
+++ TestTwo.java
@@ -0,0 +1,9 @@
+import java.time.LocalTime;
+
+public class TestTwo {
+    LocalTime t1 = LocalTime.of(5,25);
+    LocalTime t2 = LocalTime.of(5,25, 60);
+    LocalTime t3 = LocalTime.of(5,25, 60,100);
+    LocalTime t4 = LocalTime.now();
+
+}
